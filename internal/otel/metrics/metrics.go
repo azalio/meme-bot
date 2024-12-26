e9b99cab2bcb143af4e6b96bd91ec2cc97ebb2ed
Mikhail [azalio] Petrov <azalio@azalio.net>
2024-12-26 21:46:38 +0300

    meme bot v0.1.16

--- internal/otel/metrics/metrics.go
+++ internal/otel/metrics/metrics.go
@@ -1,6 +1,7 @@
 package metrics
 
 import (
+	"context"
 	"log"
 	"net/http"
 
@@ -8,18 +9,34 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
-// initMetrics инициализирует метрики и экспортер Prometheus.
+// InitMetrics инициализирует метрики и экспортер Prometheus.
 func InitMetrics() (*metric.MeterProvider, error) {
-
 	exporter, err := prometheus.New()
+	if err != nil {
+		return nil, err
+	}
+
+	// Создаем ресурс с информацией о сервисе и SDK.
+	res, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("meme-bot"),             // Указываем имя сервиса
+			semconv.TelemetrySDKLanguageGo,                        // Указываем язык SDK (Go)
+			semconv.TelemetrySDKNameKey.String("opentelemetry"),   // Указываем имя SDK (opentelemetry)
+			semconv.TelemetrySDKVersionKey.String(otel.Version()), // Указываем версию SDK
+		),
+	)
 	if err != nil {
 		return nil, err
 	}
 
 	provider := metric.NewMeterProvider(
 		metric.WithReader(exporter),
+		metric.WithResource(res), // Передаем ресурс в MeterProvider
 	)
 
 	otel.SetMeterProvider(provider)
@@ -27,7 +44,7 @@ func InitMetrics() (*metric.MeterProvider, error) {
 	return provider, nil
 }
 
-// startMetricsServer запускает HTTP сервер для экспорта метрик Prometheus.
+// StartMetricsServer запускает HTTP сервер для экспорта метрик Prometheus.
 func StartMetricsServer() {
 	http.Handle("/metrics", promhttp.Handler())
 	go func() {
